001bef5d4cca297bbfc85bb38bb53accd4b9bf8e
Dalton Dayton <daltondayton1@gmail.com>
2023-09-01 17:18:49 -0600

    Scope

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -1,9 +1,13 @@
 #include "Person.h"
+#include <iostream>
 
 int main()
 {
   Person p1("Dalton", "Dayton", 123);
-  Person p2;
+  {
+    Person p2;
+  }
+  std::cout << "after innermost block" << std::endl;
   std::string name = p1.getName();
 
   // int i = p1.arbitraryNumber;
