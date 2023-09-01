00f9a57e0809bdd537abb1c9fbe65fc73fe5f2f6
Dalton Dayton <daltondayton1@gmail.com>
2023-09-01 16:38:49 -0600

    init

---
+++ Person.cpp
@@ -0,0 +1,14 @@
+#include "Person.h"
+
+std::string Person::getName()
+{
+  return firstName + " " + lastName;
+}
+
+// int main()
+// {
+//   // Person p1 = new Person();
+//   // p1.getName();
+
+//   return 0;
+// };
